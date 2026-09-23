afea0eeb2b58a8a45a7b2056ffe77c2d00c8b5ec
Edward-Elric-5553 <edward.elric.5553@gmail.com>
2026-09-23 08:35:56 +0800

    fix(display): keep portrait text rows within eight pixels

--- boards/shields/cornix_dongle_display/custom_status_screen.c
+++ boards/shields/cornix_dongle_display/custom_status_screen.c
@@ -63,7 +63,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_color(&global_style, lv_color_black());
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
     lv_style_set_text_letter_space(&global_style, 0);
-    lv_style_set_text_line_space(&global_style, 1);
+    lv_style_set_text_line_space(&global_style, 0);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
     zmk_widget_output_status_init(&output_status_widget, screen);
